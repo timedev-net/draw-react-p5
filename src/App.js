7ed72924cf3fe42ecdf87cd7b3dd7a67b0b34506
Daniel de Brito Frota <danielfrot@gmail.com>
2022-08-11 21:59:41 -0400

    att

--- src/App.js
+++ src/App.js
@@ -11,15 +11,25 @@ let y = 50;
 		// use parent to render the canvas in this ref
 		// (without that p5 will render the canvas outside of your component)
 		p5.createCanvas(500, 500).parent(canvasParentRef);
+    p5.background(230);
+    p5.strokeWeight(2);  
 	};
 
 	const draw = (p5) => {
-		p5.background(0);
-		p5.ellipse(x, y, 70, 70);
+		// p5.background(0);
+		// p5.ellipse(x, y, 70, 70);
 		// NOTE: Do not use setState in the draw function or in functions that are executed
 		// in the draw function...
 		// please use normal variables or class properties for these purposes
-		x++;
+		// x++;
+
+    if (p5.mouseIsPressed) {
+      p5.stroke(255);
+    } else {
+      p5.stroke(237, 34, 93);
+    }
+    p5.line(p5.mouseX - 66, p5.mouseY, p5.mouseX + 66, p5.mouseY);
+    p5.line(p5.mouseX, p5.mouseY - 66, p5.mouseX, p5.mouseY + 66);
 	};
   return (
     <div className="App">
